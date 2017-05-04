9f5fd646bd6f1aa3447f5cd70624820811350aff
akihito.nakano <sora.akatsuki@gmail.com>
2017-05-04 12:08:53 +0900

    整形

--- src/models/class-product.php
+++ src/models/class-product.php
@@ -2,23 +2,23 @@
 namespace ColorMeShop\Models;
 
 class Product {
-    /**
-     * @var array
-     */
-    private $apiResponse;
+	/**
+	 * @var array
+	 */
+	private $api_response;
 
-    /**
-     * @param array $apiResponse
-     */
-	public function __construct( $apiResponse ) {
-        $this->apiResponse = $apiResponse;
-    }
+	/**
+	 * @param array $api_response
+	 */
+	public function __construct( $api_response ) {
+		$this->api_response = $api_response;
+	}
 
 	/**
 	 * @param string $name
 	 * @return mixed
 	 */
 	public function __get( $name ) {
-		return isset( $this->apiResponse['product'][$name]) ? $this->apiResponse['product'][$name] : null;
+		return isset( $this->api_response['product'][ $name ] ) ? $this->api_response['product'][ $name ] : null;
 	}
 }
