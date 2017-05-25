6ab8b8b774498d7f34186df225c9a7b9ea8be4b1
akihito.nakano <sora.akatsuki@gmail.com>
2017-05-25 22:13:54 +0900

    商品画像用のショートコードを追加

---
+++ src/shortcodes/product/class-image.php
@@ -0,0 +1,53 @@
+<?php
+namespace ColorMeShop\Shortcodes\Product;
+
+use ColorMeShop\Shortcode_Interface;
+
+class Image implements Shortcode_Interface {
+	/**
+	 * @return string
+	 */
+	public static function name() {
+		return 'colormeshop_product_image';
+	}
+
+	/**
+	 * @param \Pimple\Container $container
+	 * @param array $atts
+	 * @param string $content
+	 * @param string $tag
+	 * @return string
+	 */
+	public static function show( $container, $atts, $content, $tag ) {
+		$filtered_atts = shortcode_atts(
+			[
+				'product_id' => $container['target_id'],
+			],
+			$atts
+		);
+
+		try {
+			$product = $container['model.product_api']->fetch( $filtered_atts['product_id'] );
+		} catch ( \RuntimeException $e ) {
+			if ( $container['WP_DEBUG_LOG'] ) {
+				error_log( $e );
+			}
+			return '';
+		}
+
+		if ( ! $product->image_url ) {
+			return '';
+		}
+
+		$attributes = '';
+		foreach ( array_diff( $atts, $filtered_atts ) as $k => $v ) {
+			$attributes .= sprintf( ' %s="%s"', $k, $v );
+		}
+
+		return sprintf(
+			'<img src="%s"%s />',
+			$product->image_url,
+			$attributes
+		);
+	}
+}
