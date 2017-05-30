6768a788f565f825194b8ce61f109d0a03077bb7
ackintosh <sora.akatsuki@gmail.com>
2017-05-30 14:51:28 +0900

    商品オプションのショートコードを追加

---
+++ src/shortcodes/product/class-option.php
@@ -0,0 +1,63 @@
+<?php
+namespace ColorMeShop\Shortcodes\Product;
+
+use ColorMeShop\Shortcode_Interface;
+
+class Option implements Shortcode_Interface {
+
+	/**
+	 * @return string
+	 */
+	public static function name() {
+		return 'colormeshop_product_option';
+	}
+
+	/**
+	 * @param \Pimple\Container $container
+	 * @param array $atts
+	 * @param string $content
+	 * @param string $tag
+	 * @return string
+	 */
+	public static function show( $container, $atts, $content, $tag ) {
+		$filtered_atts = shortcode_atts(
+			[
+				'product_id' => $container['target_id'],
+				'index' => 1,
+				'data' => 'title',
+			],
+			$atts
+		);
+
+		try {
+			$product = $container['model.product_api']->fetch( $filtered_atts['product_id'] );
+		} catch ( \RuntimeException $e ) {
+			if ( $container['WP_DEBUG_LOG'] ) {
+				error_log( $e );
+			}
+			return '';
+		}
+
+		if ( ! $product->variants || ! isset( $product->variants[ $filtered_atts['index'] - 1 ] ) ) {
+			return '';
+		}
+
+		$v = $product->variants[ $filtered_atts['index'] - 1 ];
+		if ( ! isset( $v[ $filtered_atts['data'] ] ) ) {
+			return '';
+		}
+
+		switch ( $filtered_atts['data'] ) {
+			case 'stocks':
+				return number_format( $v[ $filtered_atts['data'] ] ) . $product->unit;
+				break;
+			case 'option_price':
+			case 'option_members_price':
+				return number_format( $v[ $filtered_atts['data'] ] );
+				break;
+			default:
+				return $v[ $filtered_atts['data'] ];
+				break;
+		}
+	}
+}
