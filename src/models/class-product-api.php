33f89f506f6def1e22dd7dd7f714b3b6229e94b5
ackintosh <sora.akatsuki@gmail.com>
2017-06-01 18:19:17 +0900

    サイトマップ出力クラスを追加

--- src/models/class-product-api.php
+++ src/models/class-product-api.php
@@ -62,4 +62,27 @@ class Product_Api {
 
 		return $content;
 	}
+
+	/**
+	 * @return array
+	 * @throws \RuntimeException
+	 */
+	public function fetchAll() {
+		$url      = 'https://api.shop-pro.jp/v1/products.json';
+		$response = wp_remote_get( $url, [
+			'headers' => [
+				'Authorization' => 'Bearer ' . $this->token,
+			],
+		] );
+		if ( is_wp_error( $response ) || 200 !== $response['response']['code'] ) {
+			throw new \RuntimeException( '商品情報取得に失敗しました.' );
+		}
+
+		$content  = json_decode( $response['body'], true );
+		if ( ! $content ) {
+			throw new \RuntimeException( '商品情報のデコードに失敗しました.' );
+		}
+
+		return $content;
+	}
 }
