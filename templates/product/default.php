f6eb07dd18e0ffd15a1a04ce772358b93b63672f
Kenichi Takahashi <kenichi.taka@gmail.com>
2017-06-01 12:05:49 +0900

    reformat code

--- templates/product/default.php
+++ templates/product/default.php
@@ -1,6 +1,6 @@
 <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-        <h2><?php echo $product->name ?> </h2>
+	<main id="main" class="site-main" role="main">
+		<h2><?php echo $product->name ?> </h2>
 		<?php echo $product->expl ?><br/>
 		<br/>
 		価格: <?php echo $product->sales_price ?> 円 <br/>
