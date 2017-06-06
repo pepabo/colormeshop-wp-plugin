e0e4c271e5936c1000f8def7cf9e5fc1f16dcdb3
ackintosh <sora.akatsuki@gmail.com>
2017-06-06 12:03:00 +0900

    php5.6 未満の場合にエラーメッセージを表示する

--- colormeshop-wp-plugin.php
+++ colormeshop-wp-plugin.php
@@ -8,7 +8,31 @@
  * Author URI: https://pepabo.com/
  * License: GPL2
  */
+if ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
+	add_action( 'admin_notices', 'colormeshop_wp_plugin_requirements_error' );
+	return;
+}
+
 require_once( 'vendor/autoload.php' );
 
 $p = new \ColorMeShop\Plugin;
 $p->register();
+
+/**
+ * 動作条件に満たない場合のエラーメッセージを出力する
+ *
+ * @return void
+ */
+function colormeshop_wp_plugin_requirements_error() {
+	$v = PHP_VERSION;
+	echo <<<__EOS__
+<div class="error">
+	<p>
+		カラーミーショップ Wordpress プラグインは PHP5.6 以上が必要ですが、ご利用中のバージョンは <strong>{$v}</strong> です。<br />
+		現在、プラグインは動作を停止していますので、PHP のアップデート後に再度プラグインを有効化してください。<br />
+		ご利用お待ちしています！
+	</p>
+</div>
+__EOS__;
+
+}
