e9813c46aa7a2d598374df48448f0b2d28642b6e
ackintosh <sora.akatsuki@gmail.com>
2017-06-08 17:04:52 +0900

    php-vcr 利用にあわせてテストを調整

--- tests/src/models/class-sitemap-test.php
+++ tests/src/models/class-sitemap-test.php
@@ -2,35 +2,10 @@
 namespace ColorMeShop\Models;
 
 class Sitemap_Test extends \WP_UnitTestCase {
-
-	/**
-	 * @var Product_Api
-	 */
-	private $api;
-
-	public function setUp() {
-		$this->api = $this->getMockBuilder( '\ColorMeShop\Models\Product_Api' )
-			->setConstructorArgs( [ 'dummy_token' ] )
-			->setMethods( [ 'fetch_all' ] )
-			->getMock();
-		$this->api->expects( $this->any() )
-			->method( 'fetch_all' )
-			->willReturn([
-				'products' => [
-					[
-						'id' => 1,
-						'update_date' => '2017-01-01 00:00:00',
-					],
-					[
-						'id' => 2,
-						'update_date' => '2017-01-02 00:00:00',
-					],
-				],
-			]);
-	}
-
 	/**
 	 * @test
+	 * @group phpvcr
+	 * @vcr models/sitemap/output_default_permalink.yml
 	 */
 	public function output_商品ページのパーマリンクがデフォルトの場合() {
 		// URL パラメータに colorme_item を追加する
@@ -38,14 +13,14 @@ class Sitemap_Test extends \WP_UnitTestCase {
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://example.com/?p=123&amp;colorme_item=1</loc>
-    <lastmod>2017-01-01T00:00:00+00:00</lastmod>
+    <loc>https://example.com/?p=123&amp;colorme_item=118515509</loc>
+    <lastmod>2017-05-30T05:45:30+00:00</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>
   <url>
-    <loc>https://example.com/?p=123&amp;colorme_item=2</loc>
-    <lastmod>2017-01-02T00:00:00+00:00</lastmod>
+    <loc>https://example.com/?p=123&amp;colorme_item=117182895</loc>
+    <lastmod>2017-06-05T01:23:54+00:00</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>
@@ -53,15 +28,21 @@ class Sitemap_Test extends \WP_UnitTestCase {
 
 __EOS__;
 
+		$container = _get_container();
+		$container['token'] = function ( $c ) {
+			return 'dummy';
+		};
 		$sitemap = new Sitemap(
 			'https://example.com/?p=123',// デフォルト
-			$this->api
+			$container['model.product_api']
 		);
 		$this->assertSame( $expected, $sitemap->output() );
 	}
 
 	/**
 	 * @test
+	 * @group phpvcr
+	 * @vcr models/sitemap/output_customized_permalink.yml
 	 */
 	public function output_商品ページのパーマリンクがカスタマイズされている場合() {
 		// URL パラメータを追加する
@@ -69,14 +50,14 @@ __EOS__;
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://example.com/shop/?colorme_item=1</loc>
-    <lastmod>2017-01-01T00:00:00+00:00</lastmod>
+    <loc>https://example.com/shop/?colorme_item=118515509</loc>
+    <lastmod>2017-05-30T05:45:30+00:00</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>
   <url>
-    <loc>https://example.com/shop/?colorme_item=2</loc>
-    <lastmod>2017-01-02T00:00:00+00:00</lastmod>
+    <loc>https://example.com/shop/?colorme_item=117182895</loc>
+    <lastmod>2017-06-05T01:23:54+00:00</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
   </url>
@@ -84,9 +65,13 @@ __EOS__;
 
 __EOS__;
 
+		$container = _get_container();
+		$container['token'] = function ( $c ) {
+			return 'dummy';
+		};
 		$sitemap = new Sitemap(
 			'https://example.com/shop/',
-			$this->api
+			$container['model.product_api']
 		);
 		$this->assertSame( $expected, $sitemap->output() );
 	}
