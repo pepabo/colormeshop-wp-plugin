6dd42e9c4e2fd9089c9fed5207585f850071f792
ackintosh <sora.akatsuki@gmail.com>
2017-06-23 15:49:01 +0900

    ページャ クラスを追加

---
+++ src/class-paginator.php
@@ -0,0 +1,48 @@
+<?php
+namespace ColorMeShop;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class Paginator {
+
+	/**
+	 * @var LengthAwarePaginator
+	 */
+	private $paginator;
+
+	/**
+	 * @param array $search_params
+	 * @param array $response
+	 */
+	public function __construct( $search_params, $response ) {
+		$pager_params = $search_params;
+		$pager_params['colorme_page'] = 'items';
+		unset( $pager_params['offset'] );
+
+		$this->paginator = new LengthAwarePaginator(
+			$response['products'],
+			$response['meta']['total'],
+			$response['meta']['limit'],
+			get_query_var( 'page_no' ),
+			[
+				'path' => get_permalink(),
+				'query' => $pager_params,
+				'pageName' => 'page_no',
+			]
+		);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function links() {
+		return $this->paginator->links();
+	}
+
+	/**
+	 * @return array
+	 */
+	public function data() {
+		return $this->paginator->toArray()['data'];
+	}
+}
