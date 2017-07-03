05d892d9adcf3a5556505ecef1db181228af5e10
ackintosh <sora.akatsuki@gmail.com>
2017-07-03 16:34:42 +0900

    テストを追加

---
+++ tests/src/class-plugin-test.php
@@ -0,0 +1,29 @@
+<?php
+namespace ColorMeShop;
+
+class Plugin_Test extends \WP_UnitTestCase {
+	/**
+	 * @test
+	 */
+	public function is_valid_product_page_id() {
+		$post_id = $this->factory->post->create( [
+			'post_title' => '記事テスト',
+			'post_content' => 'テスト',
+			'post_type' => 'post',
+		] );
+		$page_id = $this->factory->post->create( [
+			'post_title' => '固定ページテスト',
+			'post_content' => 'テスト',
+			'post_type' => 'page',
+		] );
+
+		$plugin = new Plugin;
+
+		$this->assertFalse( $plugin->is_valid_product_page_id( null ) );
+		$this->assertFalse( $plugin->is_valid_product_page_id( '' ) );
+		$this->assertFalse( $plugin->is_valid_product_page_id( 0 ) );
+		$this->assertFalse( $plugin->is_valid_product_page_id( 'TEST' ) );
+		$this->assertFalse( $plugin->is_valid_product_page_id( $post_id ) );
+		$this->assertTrue( $plugin->is_valid_product_page_id( $page_id ) );
+	}
+}
