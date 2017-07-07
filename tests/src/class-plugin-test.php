167d471c1a967e2b8c90e77450d3f6b44c29c7b6
ackintosh <sora.akatsuki@gmail.com>
2017-07-07 14:20:46 +0900

    add_query_vars() のテストを追加

--- tests/src/class-plugin-test.php
+++ tests/src/class-plugin-test.php
@@ -5,6 +5,22 @@ class Plugin_Test extends \WP_UnitTestCase {
 	/**
 	 * @test
 	 */
+	public function add_query_vars() {
+		$plugin = new Plugin;
+		$query_vars = $plugin->add_query_vars( [] );
+
+		$this->assertContains( 'colorme_item', $query_vars );
+		$this->assertContains( 'colorme_sitemap', $query_vars );
+		$this->assertContains( 'colorme_page', $query_vars );
+		$this->assertContains( 'category_id_big', $query_vars );
+		$this->assertContains( 'category_id_small', $query_vars );
+		$this->assertContains( 'offset', $query_vars );
+		$this->assertContains( 'page_no', $query_vars );
+	}
+
+	/**
+	 * @test
+	 */
 	public function is_valid_product_page_id() {
 		$post_id = $this->factory->post->create( [
 			'post_title' => '記事テスト',
