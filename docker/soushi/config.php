d124582c10e5193e562afa76ec8c19347f761c6e
akihito.nakano <sora.akatsuki@gmail.com>
2017-07-08 10:30:36 +0900

    apache を使う

---
+++ docker/soushi/config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'site_title'   => 'カラーミーショップ公式 WordPress プラグイン',
+    'template_dir' => '/home/colormeshop-wp-plugin/doc-templates',
+    'source_dir'   => '/home/colormeshop-wp-plugin/doc-sources',
+];
