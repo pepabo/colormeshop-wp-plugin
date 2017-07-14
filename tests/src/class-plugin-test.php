3765e39435c2b03e6536642b65d026ec9aa44dc9
ackintosh <sora.akatsuki@gmail.com>
2017-07-14 18:55:28 +0900

    404 表示のテストを追加

--- tests/src/class-plugin-test.php
+++ tests/src/class-plugin-test.php
@@ -16,4 +16,27 @@ class Plugin_Test extends \WP_UnitTestCase {
 		$this->assertContains( 'offset', $query_vars );
 		$this->assertContains( 'page_no', $query_vars );
 	}
+
+	/**
+	 * @test
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function handle_template_redirect_404ページを表示する() {
+		$page = $this->factory->post->create_and_get( [
+			'post_title' => 'テストページ',
+			'post_type'  => 'page',
+		] );
+		$this->go_to( '/?page_id=' . $page->ID );
+
+		_get_container()['model.setting']->update( [
+			'product_page_id' => $page->ID,
+		] );
+
+		$plugin = new Plugin;
+		$plugin->handle_template_redirect();
+
+		global $wp_query;
+		$this->assertTrue( $wp_query->is_404 );
+	}
 }
