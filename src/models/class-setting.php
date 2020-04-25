5ac3020e637306a95d99342f47a4e4b976e47f85
Kenichi Takahashi <kenichi.taka@gmail.com>
2020-04-25 20:19:41 +0900

    fix phpcs

--- src/models/class-setting.php
+++ src/models/class-setting.php
@@ -11,7 +11,7 @@ class Setting {
 	 */
 	public function get( $name = null ) {
 		$setting = get_option( self::KEY );
-		if ( $name === null ) {
+		if ( is_null( $name ) ) {
 			return $setting ? $setting : array();
 		}
 
