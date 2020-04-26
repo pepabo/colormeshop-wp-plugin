e828b0d172a90e5895aeb75d0a161884c013547b
Kenichi Takahashi <kenichi.taka@gmail.com>
2020-04-26 13:06:59 +0900

    Run the test with docker-compose run

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-source ./wp.env
-docker exec -i colormeshop_wp_plugin_wordpress bash -c "cd /var/www/html/wp-content/plugins/colormeshop-wp-plugin \
-&& ./bin/install-wp-tests.sh unit_test root $MYSQL_ROOT_PASSWORD mysql \
-&& ./vendor/bin/phpunit"
+docker-compose run --rm wordpress bash -c "cd /var/www/html/wp-content/plugins/colormeshop-wp-plugin \
+&& ./bin/install-wp-tests.sh unit_test root \$MYSQL_ROOT_PASSWORD mysql \
+|| ./vendor/bin/phpunit"
