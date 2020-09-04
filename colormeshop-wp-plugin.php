6f56f8fb5fa2744cd24e2cbe32eae157df895088
Kenichi Takahashi <kenichi.taka@gmail.com>
2020-09-04 21:47:29 +0900

    s/Wordpress/WordPress/g

--- colormeshop-wp-plugin.php
+++ colormeshop-wp-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: ColorMeShop Wordpress Plugin
+ * Plugin Name: ColorMeShop WordPress Plugin
  * Plugin URI: https://github.com/pepabo/colormeshop-wp-plugin
- * Description: カラーミーショップ Wordpress プラグインはWordpressでオンラインショップを構築することができるプラグインです。商品管理やショッピングカートの機能はカラーミーショップを利用することで、本格的なオンラインショップ構築をWordpressで行うことができます。
+ * Description: カラーミーショップ WordPress プラグインはWordPressでオンラインショップを構築することができるプラグインです。商品管理やショッピングカートの機能はカラーミーショップを利用することで、本格的なオンラインショップ構築をWordPressで行うことができます。
  * Version: 2.0.0
  * Author: GMO Pepabo, Inc.
  * Author URI: https://pepabo.com/
@@ -28,7 +28,7 @@ function colormeshop_wp_plugin_requirements_error() {
 	echo <<<__EOS__
 <div class="error">
 	<p>
-		カラーミーショップ Wordpress プラグインは PHP7.3 以上が必要ですが、ご利用中のバージョンは <strong>{$v}</strong> です。<br />
+		カラーミーショップ WordPress プラグインは PHP7.3 以上が必要ですが、ご利用中のバージョンは <strong>{$v}</strong> です。<br />
 		現在、プラグインは動作を停止していますので、PHP のアップデート後に再度プラグインを有効化してください。<br />
 		ご利用お待ちしています！
 	</p>
