5bd93d1cc3f9c480434dd7dde506e70ff9b06b0a
shiorin <32048522+ShioriPeace@users.noreply.github.com>
2024-08-13 19:04:09 +0900

    追加した部分が動くことを担保するテストを追加

--- tests/src/shortcodes/product/class-page-test.php
+++ tests/src/shortcodes/product/class-page-test.php
@@ -25,4 +25,34 @@ class Page_Test extends \WP_UnitTestCase {
 			)
 		);
 	}
+
+	/**
+	 * @test
+	 */
+	public function show_存在しない商品IDが指定された場合は、ApiExceptionをcatchし空文字を返す() {
+		$container = $this->createMock(\Pimple\Container::class);
+		$product_api = $this->createMock(\ColorMeShop\Swagger\Api\ProductApi::class);
+
+		$product_api->method('fetch')
+			->willThrowException(new \ColorMeShop\Swagger\ApiException());
+
+		$container->method('offsetGet')
+			->willReturnMap([
+				['api.product_api', $product_api],
+				['WP_DEBUG_LOG', false],
+			]);
+
+		$this->assertSame(
+			'',
+			Page::show(
+				$container,
+				[
+					'template' => 'default',
+					'product_id' => '999999', // 存在しない商品ID
+				],
+				null,
+				null
+			)
+		);
+	}
 }
